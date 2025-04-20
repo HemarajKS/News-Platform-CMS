13efe655c9f69aeec284e1e5bf5e42cf9b29c739
hemaraja.shettigar <hemaraja.shettigar@robosoftin.com>
2025-04-20 08:27:23 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Dashboard from "./pages/Dashboard";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import ArticleManager from "./components/ArticleManager";
 import AuthorManager from "./components/AuthorManager";
 import CategoryManager from "./components/CategoryManager";
 
 const App: React.FC = () => {
+  const location = useLocation();
+
+  //On route change scroll to top
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
   return (
     <Routes>
       <Route path="/" element={<Dashboard />}>
         <Route index element={<ArticleManager />} />
-        {/* <Route path="articles" element={<ArticleManager />} /> */}
         <Route path="authors" element={<AuthorManager />} />
         <Route path="categories" element={<CategoryManager />} />
       </Route>
